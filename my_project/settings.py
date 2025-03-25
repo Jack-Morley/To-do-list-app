9f15bd768606b372053d7e82e76e64f539b24a0f
Jack Morley <jdmorley929@gmail.com>
2025-03-25 15:55:42 +0000

    update database configuration and adjust static URL path in settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 #    }
 #}
 DATABASES = {
-   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
